65ec1ca9e342d3c64b80dacc836bad200d2e0ecd
nullptr02 <mesteves@my.yorku.ca>
2022-10-27 15:23:26 -0400

    Completed the Content View

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Michael on 2022-10-26.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
